64b87d34ca4382d74b713c757f60ecc80e88967c
bobokulov <86780504+bobokulov@users.noreply.github.com>
2021-08-05 20:38:33 +0200

    test

--- src/sidebord.js
+++ src/sidebord.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class sidebord extends Component {
     render() {
-        return <div>Siedebord</div>
+        return (
+            <div>
+                
+            </div>
+        )
     }
 }
